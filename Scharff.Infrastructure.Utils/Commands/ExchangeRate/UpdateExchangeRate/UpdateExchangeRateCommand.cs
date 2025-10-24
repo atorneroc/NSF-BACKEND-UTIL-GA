507ab8385f93793b4fa2193ca1b80b805640583b
Alfredo Tornero <e_aacarlos@stefanini.com>
2025-10-24 16:09:00 -0500

    ajustar pipelines

--- Scharff.Infrastructure.Utils/Commands/ExchangeRate/UpdateExchangeRate/UpdateExchangeRateCommand.cs
+++ Scharff.Infrastructure.Utils/Commands/ExchangeRate/UpdateExchangeRate/UpdateExchangeRateCommand.cs
@@ -22,13 +22,14 @@ namespace Scharff.Infrastructure.PostgreSQL.Commands.ExchangeRate.UpdateExchange
                 try
                 {
 
-                    string insert =
-                        "UPDATE nsf.tipo_cambio SET " +
-                        "compra=" + billableOrder.bank_purchase +
-                        ", venta=" + billableOrder.bank_sale +
-                        ", usuario_modificacion='" + billableOrder.modification_author + "'" +
-                        ", fecha_modificacion=(SELECT current_timestamp AT TIME ZONE 'America/Lima')" +
-                        " WHERE id=" + billableOrder.id + ";";
+                    string insert = @"  UPDATE 
+		                                    nsf.tipo_cambio
+	                                    SET  
+		                                    compra=@bank_purchase, 
+		                                    venta=@bank_sale, 
+		                                    usuario_modificacion=@modification_author, 
+		                                    fecha_modificacion=(SELECT current_timestamp AT TIME ZONE 'America/Lima')
+	                                    WHERE id =@id;";
 
                     int idInsert = await connection.ExecuteScalarAsync<int>(insert, billableOrder);
                     trans.Complete();
@@ -41,20 +42,5 @@ namespace Scharff.Infrastructure.PostgreSQL.Commands.ExchangeRate.UpdateExchange
                 }
             }
         }
-        
-        // --- METODO DE PRUEBA (temporal) PARA DISPARAR CODEQL ---
-        public async Task<int> UpdateExchangeRateUnsafe(string userInput)
-        {
-            // Ejemplo intencionalmente inseguro: concatenación directa de entrada externa
-            string unsafeSql = "UPDATE nsf.tipo_cambio SET compra = " + userInput + " WHERE id = 1;";
-
-            using (IDbConnection connection = new NpgsqlConnection(_connection.ConnectionString))
-            {
-                // execute with concatenated SQL (inseguro)
-                var result = await connection.ExecuteScalarAsync<int>(unsafeSql);
-                return result;
-            }
-        }
-
     }
 }
